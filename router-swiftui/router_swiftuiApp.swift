3b0ea7fdfa738f45470645337803c4923a107d84
Jeffrey Jackson <jj@autolean.com>
2023-11-28 09:37:43 -0500

    Initial Commit

---
+++ router-swiftui/router_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  router_swiftuiApp.swift
+//  router-swiftui
+//
+//  Created by Jeffrey Jackson on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct router_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
